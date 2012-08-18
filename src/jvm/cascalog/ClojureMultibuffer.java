8c59e156d356178abc4b552dbdf7494bbd9ab1c9
Spencer Tipping <spencer@spencertipping.com>
2012-08-17 17:02:38 -0700

    Removed state support, added include_context instead (no clojure modifications yet)

--- src/jvm/cascalog/ClojureMultibuffer.java
+++ src/jvm/cascalog/ClojureMultibuffer.java
@@ -1,14 +1,14 @@
 /*
     Copyright 2010 Nathan Marz
- 
-    Project and contact information: http://www.cascalog.org/ 
+
+    Project and contact information: http://www.cascalog.org/
 
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
     You may obtain a copy of the License at
-   
+
         http://www.apache.org/licenses/LICENSE-2.0
-   
+
     Unless required by applicable law or agreed to in writing, software
     distributed under the License is distributed on an "AS IS" BASIS,
     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -30,8 +30,8 @@ import java.util.List;
 
 public class ClojureMultibuffer extends ClojureCascadingBase implements MultiBuffer {
 
-    public ClojureMultibuffer(Fields out_fields, Object[] fn_spec, boolean stateful) {
-        super(out_fields, fn_spec, stateful);
+    public ClojureMultibuffer(Fields out_fields, Object[] fn_spec, boolean include_context) {
+        super(out_fields, fn_spec, include_context);
     }
 
     public void operate(MultiBufferContext context) {
